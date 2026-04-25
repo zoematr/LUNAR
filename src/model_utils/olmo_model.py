6026d6cc62353dfea0033d7cc2835e112296323c
Zoe Matrullo <104142441+zoematr@users.noreply.github.com>
2026-04-25 11:41:20 +0100

    switch to float16 for local runs

---
+++ src/model_utils/olmo_model.py
@@ -0,0 +1,197 @@
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+
+import torch
+import functools
+
+from transformers import AutoTokenizer, AutoModelForCausalLM
+from typing import List
+from torch import Tensor
+from jaxtyping import Float
+
+from src.utils.utils import get_orthogonalized_matrix
+from src.model_utils.model_base import ModelBase
+
+OLMO_CHAT_TEMPLATE = """<|user|>
+{instruction}
+<|assistant|>
+"""
+
+OLMO_REFUSAL_TOKS = [40, 2170]  # ['I', 'As']
+
+
+def format_instruction_olmo_chat(
+    instruction: str,
+    output: str = None,
+    include_trailing_whitespace: bool = True,
+):
+    formatted_instruction = OLMO_CHAT_TEMPLATE.format(instruction=instruction)
+
+    if not include_trailing_whitespace:
+        formatted_instruction = formatted_instruction.rstrip()
+
+    if output is not None:
+        formatted_instruction += output
+
+    return formatted_instruction
+
+
+def tokenize_instructions_olmo_chat(
+    tokenizer: AutoTokenizer,
+    instructions: List[str],
+    outputs: List[str] = None,
+    include_trailing_whitespace=True,
+):
+    if outputs is not None:
+        prompts = [
+            format_instruction_olmo_chat(
+                instruction=instruction,
+                output=output,
+                include_trailing_whitespace=include_trailing_whitespace,
+            )
+            for instruction, output in zip(instructions, outputs)
+        ]
+    else:
+        prompts = [
+            format_instruction_olmo_chat(
+                instruction=instruction,
+                include_trailing_whitespace=include_trailing_whitespace,
+            )
+            for instruction in instructions
+        ]
+
+    result = tokenizer(
+        prompts,
+        padding=True,
+        truncation=False,
+        return_tensors="pt",
+    )
+
+    return result
+
+
+def orthogonalize_olmo_weights(model, direction: Float[Tensor, "d_model"]):
+    model.model.embed_tokens.weight.data = get_orthogonalized_matrix(
+        model.model.embed_tokens.weight.data, direction
+    )
+
+    for block in model.model.layers:
+        block.self_attn.o_proj.weight.data = get_orthogonalized_matrix(
+            block.self_attn.o_proj.weight.data.T, direction
+        ).T
+        block.mlp.down_proj.weight.data = get_orthogonalized_matrix(
+            block.mlp.down_proj.weight.data.T, direction
+        ).T
+
+
+def act_add_olmo_weights(model, direction: Float[Tensor, "d_model"], coeff, layer):
+    dtype = model.model.layers[layer - 1].mlp.down_proj.weight.dtype
+    device = model.model.layers[layer - 1].mlp.down_proj.weight.device
+
+    bias = (coeff * direction).to(dtype=dtype, device=device)
+
+    model.model.layers[layer - 1].mlp.down_proj.bias = torch.nn.Parameter(bias)
+
+
+class OLMoModel(ModelBase):
+
+    def _load_model(self, model_path, dtype=torch.float16):
+        model = AutoModelForCausalLM.from_pretrained(
+            model_path,
+            dtype=dtype,
+            trust_remote_code=True,
+            device_map="auto",
+        ).eval()
+
+        model.requires_grad_(False)
+
+        return model
+
+    def _load_tokenizer(self, model_path):
+        tokenizer = AutoTokenizer.from_pretrained(model_path)
+
+        tokenizer.padding_side = "left"
+        tokenizer.pad_token = tokenizer.eos_token
+        tokenizer.pad_token_id = tokenizer.eos_token_id
+
+        return tokenizer
+
+    def _get_tokenize_instructions_fn(self):
+        return functools.partial(
+            tokenize_instructions_olmo_chat,
+            tokenizer=self.tokenizer,
+            include_trailing_whitespace=True,
+        )
+
+    def _get_eoi_toks(self):
+        return self.tokenizer.encode(
+            OLMO_CHAT_TEMPLATE.split("{instruction}")[-1], add_special_tokens=False
+        )
+
+    def _get_refusal_toks(self):
+        return OLMO_REFUSAL_TOKS
+
+    def _get_model_block_modules(self):
+        return self.model.model.layers
+
+    def _get_attn_modules(self):
+        return torch.nn.ModuleList(
+            [block_module.self_attn for block_module in self.model_block_modules]
+        )
+
+    def _get_mlp_modules(self):
+        return torch.nn.ModuleList(
+            [block_module.mlp for block_module in self.model_block_modules]
+        )
+
+    def _get_orthogonalization_mod_fn(self, direction: Float[Tensor, "d_model"]):
+        return functools.partial(orthogonalize_olmo_weights, direction=direction)
+
+    def _get_act_add_mod_fn(self, direction: Float[Tensor, "d_model"], coeff, layer):
+        return functools.partial(
+            act_add_olmo_weights, direction=direction, coeff=coeff, layer=layer
+        )
+
+    def _to(self, device=None, dtype=None):
+        # device_map="auto" already handles placement; moving manually conflicts with accelerate hooks
+        return self
+
+    def _eval(self):
+        self.model.eval()
+        return self
+
+    def _forward(self, batch):
+        return self.model(**batch)
+
+    def _device(self):
+        return next(self.model.parameters()).device.type
+
+    def _generate(
+        self,
+        input_ids,
+        attention_mask,
+        max_length,
+        max_new_tokens,
+        do_sample,
+        num_beams,
+        num_return_sequences,
+        use_cache,
+        pad_token_id,
+        output_scores,
+        return_dict_in_generate,
+    ):
+        return self.model.generate(
+            input_ids=input_ids,
+            attention_mask=attention_mask,
+            max_length=max_length,
+            max_new_tokens=max_new_tokens,
+            do_sample=do_sample,
+            num_beams=num_beams,
+            num_return_sequences=num_return_sequences,
+            use_cache=use_cache,
+            pad_token_id=pad_token_id,
+            output_scores=output_scores,
+            return_dict_in_generate=return_dict_in_generate,
+        )
+
+    def _save_pretrained(self, save_directory):
+        self.model.save_pretrained(save_directory)
