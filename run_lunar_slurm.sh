739037b4f11fe51b856c76041f6379a5b9a32739
Zoe Matrullo <matrullo@madeira.dbs.ifi.lmu.de>
2026-05-14 11:46:20 +0200

    add results routing analysis

---
+++ run_lunar_slurm.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+#
+#SBATCH --job-name lunar-unlearn
+#SBATCH --output=/nfs/data8/matrullo/LUNAR/logs/res_%j.txt
+#SBATCH --ntasks=1
+#SBATCH --time=4:00:00
+#SBATCH --gres=gpu:1
+#SBATCH -p minor
+
+# Paths
+WORK_DIR=/nfs/data8/matrullo/LUNAR
+VENV_DIR=/nfs/data8/matrullo/venv_lunar
+
+# Activate virtual environment
+source $VENV_DIR/bin/activate
+
+# Move to project directory
+cd $WORK_DIR
+
+# Run LUNAR
+python run_lunar.py
